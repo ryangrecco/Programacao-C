04b0f51b3eb8041b576381c076a06c5662d24853
ryangrecco <ryankgrecco5@gmail.com>
2026-03-10 16:55:00 -0300

    aulas

---
+++ aula02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 23;
+    int quantidade = 1;
+    float altura = 1.90;
+    float peso = 80.00;
+    char nome[20] = 'Ryan';
+}
---
+++ aula03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 23;
+    int quantidade = 1;
+    float altura = 1.90;
+    float peso = 80.00;
+    char nome[20] = "Ryan";
+
+    printf("Meu nome %s e idade %d\n", nome, idade);
+    printf("A altura é %f\n", altura);
+}
